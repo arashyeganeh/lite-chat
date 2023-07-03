affe001f5459945247b6d2eee27b70e772d3fdb4
Arash <arashkosariyeganeh@gmail.com>
2023-07-03 16:49:20 +0330

    Add "logout" button

--- app.js
+++ app.js
@@ -5,15 +5,16 @@ const cookieParser = require('cookie-parser');
 
 const roomRoute = require(path.join(__dirname, '/routes/room'));
 const loginRoute = require(path.join(__dirname, '/routes/login'));
-const signoutRoute = require(path.join(__dirname, '/routes/signout'));
+const logoutRoute = require(path.join(__dirname, '/routes/logout'));
 
 serverService.app.use(express.urlencoded({ extended: false }));
 serverService.app.use(cookieParser())
 serverService.app.use(express.json());
+serverService.app.use(express.static('public'))
 serverService.app.use(express.static('dist'))
 
 serverService.app.use('/room', roomRoute);
-serverService.app.use('/signout', signoutRoute);
+serverService.app.use('/logout', logoutRoute);
 serverService.app.use('*', loginRoute);
 
 serverService.initServer();
